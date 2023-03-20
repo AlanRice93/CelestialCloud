bed3e82f50d0316ab2ef154d012d545239d8f86c
Alan Rice <mail@alanrice.me>
2023-03-20 18:58:15 -0400

    component layout

---
+++ src/pages/data.ts
@@ -0,0 +1,10 @@
+import { Experience } from "../types"
+
+export const experience: Record<string, Experience> = {
+  oak: {
+    title: "Oak",
+    company: "Oak"
+    date: "201666666"
+    summary: ""
+  },
+}
